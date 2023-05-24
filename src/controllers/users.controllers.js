210d3c227d016ac9486711851c16c12038303223
Giovanna <giovannad1clonius@gmail.com>
2023-05-24 18:20:52 -0300

    login pronto

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 export async function signup(req, res){
     const { name, email, photo, biography, password } = req.body;
@@ -13,6 +14,31 @@ export async function signup(req, res){
         [name, email, photo, biography, hash]);
 
         return res.sendStatus(201);
+    } catch (err) {
+        return res.status(500).send(err.message);
+    };
+};
+
+export async function signin (req, res){
+    const { email } = req.body;
+
+    try {
+        const token = uuid();
+
+        const userExists = await db.query(`SELECT * FROM users WHERE email=$1;`, [email]);
+        const id = userExists.rows[0].id;
+
+        const tokenExists = await db.query(
+            `SELECT token FROM sessions 
+            JOIN users ON sessions."userId" = users.id
+            WHERE users.id=$1;`, [id]);
+
+        if (tokenExists.rows.length !== 0) {
+            return res.status(200).send(tokenExists.rows[0]);
+        } else {
+            await db.query(`INSERT INTO sessions (token, "userId") VALUES ($1, $2);`, [token, id]);
+        };
+        return res.status(200).send({ token: token })
     } catch (err) {
         return res.status(500).send(err.message);
     };
